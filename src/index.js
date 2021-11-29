92f349a1c616000f80110b759e86a36b750c1a35
raul.jacinto <raul.seaj@gmail.com>
2021-11-29 14:44:23 -0600

    eliminar el directorio pages

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import {
   Route
 } from "react-router-dom";
 import App from "./App";
-import About from "./componentes/pages/about";
-import home from "./componentes/pages/home"
-import Contact from "./componentes/pages/contact";
+
 import Catalogos from "./componentes/Catalogos/Catalogos"
 import Dashboard from "./componentes/Admin/Dashboard/Dashboard"
 
@@ -16,8 +14,6 @@ render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
         <Route path="catalogos" element={<Catalogos />} />
         <Route path="login" element={<Dashboard />} />
       </Route>
